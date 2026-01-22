049facf84749b8f54cecb4947571c714bb36ca8e
Amit Singh <amitksingh.1204@gmail.com>
2026-01-22 11:36:33 +0530

    UI Changes

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -46,10 +46,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             <Comp
                 className={cn(buttonVariants({ variant, size, className }))}
                 ref={ref}
-        ...props
-    />
-    )
-  }
+                {...props}
+            />
+        )
+    }
 )
 Button.displayName = "Button"
 
